39616a5522996a18bdd06b5861ec1f34136ddcaf
alya <alyaggomaa@gmail.com>
2023-07-10 14:28:03 +0300

    always use output/<cur_run>/slips_output as slips output dir (using -o) when starting slips

--- parsers/slips.py
+++ parsers/slips.py
@@ -1,15 +1,41 @@
 import os
-import json
+import subprocess
+import threading
 from database.sqlite_db import SQLiteDB
 
 class SlipsParser:
-    def __init__(self, given_input: str, db: SQLiteDB):
+    def __init__(self, slips_path, given_input: str,  output_dir: str, db: SQLiteDB):
         self.given_input: str = given_input
+        # this is the output dir where this tool puts all the output files and dbs etc.
+        self.output_dir = output_dir
         self.db = db
+        # full path to slips installation dir
+        self.slips_path: str = slips_path
+        self.slips_output_dir = os.path.join(self.output_dir, 'Slips_output/')
+        # this wil be a subdir inside the output dir where slips will place all of its log files
+        os.mkdir(self.slips_output_dir)
+        self.run()
 
-    def start(self):
+
+    def parse_output(self):
+        """reads the output db of slips with the labels and stores it in this tools' db"""
+        ...
+
+
+    def run(self):
         """
         runs slips in a thread in the bg on the given file
         """
-        pass
+        slips_thread = threading.Thread(target=self.exec_slips, daemon=True)
+        slips_thread.start()
+
+        # wait for slips to finish
+        slips_thread.join()
+
+        self.parse_output()
+
+
+
+
+
 
