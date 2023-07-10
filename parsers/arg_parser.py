96c97f2ed6ecf7d8adc8dab2334677b1883665b5
alya <alyaggomaa@gmail.com>
2023-07-10 13:49:20 +0300

    support giving slips a zeek dir or a pcap

--- parsers/arg_parser.py
+++ parsers/arg_parser.py
@@ -3,7 +3,7 @@ import argparse
 
 class ArgsParser:
     parser = argparse.ArgumentParser(description='Process command line arguments.')
-    parser.add_argument('-d', '--zeekdir', dest='zeek_dir', help='Zeek directory for Slips')
+    parser.add_argument('-s', '--slips_input_file', dest='slips_input_file', help='PCAP or zeek dir for Slips')
     parser.add_argument('-e', '--eve', dest='eve_file', help='eve.json file of Suricata')
     parser.add_argument('-g', '--gt', dest='ground_truth_dir', help='Ground truth labeled Zeek directory')
     args = parser.parse_args()
