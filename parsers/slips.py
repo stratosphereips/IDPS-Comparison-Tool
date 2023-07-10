42d3617f74548b0bc8a84084c3dab4e5b1a59c7c
alya <alyaggomaa@gmail.com>
2023-07-10 14:40:33 +0300

    add a method to start slips in a separatae thread and wait for it to finish

--- parsers/slips.py
+++ parsers/slips.py
@@ -12,10 +12,33 @@ class SlipsParser:
         # full path to slips installation dir
         self.slips_path: str = slips_path
         self.slips_output_dir = os.path.join(self.output_dir, 'Slips_output/')
-        # this wil be a subdir inside the output dir where slips will place all of its log files
+        # this wil be a subdir insitde the output dir where slips will place all of its log files
         os.mkdir(self.slips_output_dir)
         self.run()
 
+    def exec_slips(self):
+        # TODO when slips is started by this tool, we should specify
+        #  the output dir in slips conf, otherwise this tools will create a dir in
+        command = [
+            './slips.py',
+            '-e', '1',
+            '-f', self.given_input,
+            '-o' , self.slips_output_dir,
+        ]
+        slips = subprocess.Popen(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            cwd=self.slips_path,
+        )
+        # you have to get the pid before communicate()
+        self.slips_pid = slips.pid
+        out, error = slips.communicate()
+        # if out:
+        #     print(f"Zeek: {out}")
+        # if error:
+        #     self.print(f"Zeek error. return code: {zeek.returncode} error:{error.strip()}")
 
     def parse_output(self):
         """reads the output db of slips with the labels and stores it in this tools' db"""
