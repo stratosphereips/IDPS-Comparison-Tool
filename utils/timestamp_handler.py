5b4a559a53dcdcfb9b3223716a3939aca6d28d02
alya <alyaggomaa@gmail.com>
2023-08-24 17:48:46 +0300

    make utils a dir instead of a file with many classes

---
+++ utils/timestamp_handler.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timezone
+
+
+class TimestampHandler():
+    def convert_iso_8601_to_unix_timestamp(self, ts: str) -> float:
+        """
+        converts iso 8601 format to unix timestamp
+        expected format: %Y-%m-%dT%H:%M:%S.%f%z
+        :param ts: ts in expected format
+        :return: the given ts in unix format
+        """
+        dt = datetime.strptime(ts, '%Y-%m-%dT%H:%M:%S.%f%z')
+
+        # convert datetime to time-aware timezone at UTC
+        # so correct timestamp is returned
+        dt = dt.replace(tzinfo=timezone.utc)
+
+        # Return the time in seconds since the epoch
+        seconds_since_epoch = dt.timestamp()
+
+        return seconds_since_epoch
+
+    def is_unix_timestamp(self, s):
+        try:
+            # Convert the string to a floating-point number
+            timestamp = float(s)
+
+            # Check if the timestamp is non-negative
+            if timestamp < 0:
+                return False
+
+            # Check if the string is composed of only digits and optional decimal point
+            if not s.replace(".", "").isdigit():
+                return False
+
+            return True
+
+        except ValueError:
+            return False
--- utils/timewindow_handler.py
+++ utils/timewindow_handler.py
@@ -1,51 +1,7 @@
 from datetime import datetime, timezone
 from math import ceil
-import communityid
 
-def get_community_id(flow: dict):
-    """
-    calculates the flow community id based on the protocol
-    """
-    community_id = communityid.CommunityID()
-    cases = {
-    'tcp': communityid.FlowTuple.make_tcp,
-    'udp': communityid.FlowTuple.make_udp,
-    'icmp': communityid.FlowTuple.make_icmp,
-    }
-
-    try:
-        proto = flow['proto'].lower()
-
-        if 'icmp' in proto:
-            tpl = cases['icmp'](flow['saddr'], flow['daddr'], flow['type'], flow['code'])
-        else:
-            tpl = cases[proto](flow['saddr'], flow['daddr'], flow['sport'], flow['dport'])
-
-        return community_id.calc(tpl)
-    except (KeyError, TypeError):
-        # proto doesn't have a community_id.FlowTuple  method
-        return ''
-
-
-def convert_iso_8601_to_unix_timestamp(ts: str) -> float:
-    """
-    converts iso 8601 format to unix timestamp
-    expected format: %Y-%m-%dT%H:%M:%S.%f%z
-    :param ts: ts in expected format
-    :return: the given ts in unix format
-    """
-    dt = datetime.strptime(ts, '%Y-%m-%dT%H:%M:%S.%f%z')
-
-    # convert datetime to time-aware timezone at UTC
-    # so correct timestamp is returned
-    dt = dt.replace(tzinfo=timezone.utc)
-
-    # Return the time in seconds since the epoch
-    seconds_since_epoch = dt.timestamp()
-
-    return seconds_since_epoch
-
-class TimewindowHandler():
+class TimewindowHandler:
     """
     Explanation of Timewindows
     if we have a pcap with the following
