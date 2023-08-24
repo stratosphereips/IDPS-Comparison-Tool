254acd81fac387ccd696a2106d12fe6c6480d17a
alya <alyaggomaa@gmail.com>
2023-08-24 14:42:24 +0300

    suricata: start the twid handler after getting the first flow ts

--- parsers/suricata.py
+++ parsers/suricata.py
@@ -1,14 +1,16 @@
+from utils import get_community_id, convert_iso_8601_to_unix_timestamp, TimewindowHandler
 from database.sqlite_db import SQLiteDB
 from termcolor import colored
 import json
 
-from .utils import get_community_id, convert_iso_8601_to_unix_timestamp
 
 class SuricataParser:
     name = "SuricataParser"
     def __init__(self, eve_file: str, db: SQLiteDB):
         self.eve_file: str = eve_file
         self.db = db
+        # to be able to get the ts of the first flow
+        self.is_first_flow = True
 
     def log(self, green_txt, normal_txt):
         normal_txt = str(normal_txt)
@@ -52,10 +54,6 @@ class SuricataParser:
                 event_type = line['event_type']
                 flows += 1
 
-                # todo handle icmp
-                # {'timestamp': '1970-01-01T02:00:00.000016+0200', 'flow_id': 806511107004851, 'event_type': 'flow', 'src_ip': 'fe80:0000:0000:0000:d5e6:502a:54ce:e387', 'dest_ip': 'ff02:0000:0000:0000:0000:0000:0000:0002', 'proto': 'IPv6-ICMP', 'icmp_type': 133, 'icmp_code': 0, 'flow': {'pkts_toserver': 3, 'pkts_toclient': 0, 'bytes_toserver': 210, 'bytes_toclient': 0, 'start': '1970-01-01T02:00:10.678323+0200', 'end': '1970-01-01T02:00:18.679102+0200', 'age': 8, 'state': 'new', 'reason': 'shutdown', 'alerted': False}, 'community_id': '1:kRuEWzfQpgjU3t+9Uf3kvfhRuA0='}
-
-                #{"timestamp":"1970-01-01T02:00:00.000016+0200","flow_id":81604379302370,"event_type":"flow","src_ip":"fe80:0000:0000:0000:d5e6:502a:54ce:e387","dest_ip":"ff02:0000:0000:0000:0000:0000:0000:0016","proto":"IPv6-ICMP","icmp_type":143,"icmp_code":0,"flow":{"pkts_toserver":2,"pkts_toclient":0,"bytes_toserver":180,"bytes_toclient":0,"start":"1970-01-01T02:00:10.678370+0200","end":"1970-01-01T02:00:11.178825+0200","age":1,"state":"new","reason":"shutdown","alerted":false},"community_id":"1:1wlgkbwn0/hDVbFA0hD7tRnEH9w="}
 
 
                 if event_type == 'stats':
@@ -63,6 +61,11 @@ class SuricataParser:
 
                 flow: dict = self.extract_flow(line)
                 timestamp = convert_iso_8601_to_unix_timestamp(flow['timestamp'])
+
+                # start the tw handler and keep track of the ts of the first tw
+                if self.is_first_flow:
+                    self.twid_handler = TimewindowHandler(ts_of_first_flow=timestamp)
+                    self.is_first_flow = False
 
                 #TODO suricata calculates the cid in a wrong way, we'll be calculating it on the fly until they fix it
                 cid: str = get_community_id(flow)
