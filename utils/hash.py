4aea7d39f2e2ffc35c020bc75186409eba3bca6e
alya <alyaggomaa@gmail.com>
2023-08-25 14:37:09 +0300

    hash.py: refactor

--- utils/hash.py
+++ utils/hash.py
@@ -1,29 +1,34 @@
 from datetime import datetime, timezone
 from hashlib import sha1
-import community_id
-from timestamp_handler import TimestampHandler
+from base64 import b64encode
+
+import communityid
+from .timestamp_handler import TimestampHandler
 
 class Hash:
     def __init__(self):
         self.community_id = communityid.CommunityID()
         self.time_handler = TimestampHandler()
 
+
     def get_aid(self, flow: dict):
         """
         calculates the flow SHA1(cid+ts) aka All-ID of the flow
         because we need the flow ids to be unique to be able to compare them
         """
+        #TODO document this
         community_id = self.get_community_id(flow)
-        ts = self.time_handler.remove_milliseconds_decimals(flow['timestamp'])
-        cid_and_ts = f"{self.community_id}-{ts}"
+        ts: str = self.time_handler.remove_milliseconds_decimals(flow['timestamp'])
 
-        # Convert the input string to bytes (since hashlib works with bytes)
-        input_bytes = cid_and_ts.encode('utf-8')
+        aid = f"{community_id}-{ts}"
 
-        return sha1(input_bytes).hexdigest()
+        # convert the input string to bytes (since hashlib works with bytes)
+        aid: str = sha1(aid.encode('utf-8')).hexdigest()
+
+        return str(b64encode(aid.encode('utf-8')))
 
 
-    def get_community_id(flow: dict):
+    def get_community_id(self, flow: dict):
         """
         calculates the flow community id of the given flow
         """
@@ -41,7 +46,7 @@ class Hash:
             else:
                 tpl = cases[proto](flow['saddr'], flow['daddr'], flow['sport'], flow['dport'])
 
-            return community_id.calc(tpl)
+            return self.community_id.calc(tpl)
         except (KeyError, TypeError):
             # proto doesn't have a community_id.FlowTuple  method
             return ''
