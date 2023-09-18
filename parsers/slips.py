8f49a6b801b4566228a37420cb97279b3bf96ad2
alya <alyaggomaa@gmail.com>
2023-09-18 17:02:06 +0300

    print a warning when unable to map a slips alert to an existing tw

--- parsers/slips.py
+++ parsers/slips.py
@@ -3,6 +3,7 @@ import sqlite3
 from database.sqlite_db import SQLiteDB
 from abstracts.abstracts import Parser
 from termcolor import colored
+from utils.timestamp_handler import TimestampHandler
 from threading import Lock
 from time import sleep
 
@@ -18,6 +19,7 @@ class SlipsParser(Parser):
         self.slips_db: str = slips_db
         # caches the labeled tws
         self.labeled_tws = []
+        self.timestamp_handler = TimestampHandler()
 
     def connect(self):
         self.conn = sqlite3.connect(self.slips_db, check_same_thread=False)
@@ -96,6 +98,25 @@ class SlipsParser(Parser):
         #     self.db.store_tw_label('slips', tw, row['label'])
         #     self.labeled_tws.append(tw)
 
+    def warn_about_discarded_alert(self, alert: dict):
+        """
+        prints a warning when the tool i sdiscarding an alert detected by slips
+        :param alert:
+        :return:
+        """
+        gt_start_time, gt_end_time = self.db.get_timewindows_limit()
+
+        gt_start_time = self.timestamp_handler.convert_to_human_readable(gt_start_time)
+        gt_end_time = self.timestamp_handler.convert_to_human_readable(gt_end_time)
+        slips_start_time =  self.timestamp_handler.convert_to_human_readable(alert['tw_start'])
+        slips_end_time =  self.timestamp_handler.convert_to_human_readable(alert['tw_end'])
+
+        self.log(f"Slips marked timewindow {alert['timewindow']} as malicious,"
+                 f"timewindow start: {slips_start_time} end: {slips_end_time}. "
+                 f"meanwhile tws in gt start at: {gt_start_time} and end at: {gt_end_time}. ",
+                 "discarding alert.")
+
+
     def parse_alerts_table(self):
         """
         parses the labels set by slips for each timewindow, and marks them as malicious in this tools' db
@@ -109,8 +130,9 @@ class SlipsParser(Parser):
 
             ts = float(ts)
             ip = ip.replace("profile_","")
-            tw_number: int = self.db.get_timewindow_of_ts(ts)
-            self.db.set_tw_label(ip, 'slips', tw_number, 'malicious')
+            if tw_number:= self.db.get_timewindow_of_ts(ts):
+                self.db.set_tw_label(ip, 'slips', tw_number, 'malicious')
+                return True
 
         for alert in self.iterate('alerts'):
             # what we're doing here is marking tw 1 and 2 as malicious if a slips alert exists in parts of both
@@ -121,9 +143,11 @@ class SlipsParser(Parser):
             # ├────────────────────────────────────┼─────────────────────────────────────┤
             # │             tw 1                   │            tw 2                     │
 
-            mark_tw_as_malicious(alert['tw_start'], alert['ip_alerted'])
-            mark_tw_as_malicious(alert['tw_end'], alert['ip_alerted'])
-
+            for ts in (alert['tw_start'], alert['tw_end']):
+                if not mark_tw_as_malicious(ts , alert['ip_alerted']):
+                    # we can't get the corresponding tw of an alert that was found in slips,
+                    # discard it and print a warning
+                    self.warn_about_discarded_alert(alert)
 
 
     def parse_flow_by_flow_labels(self):
