5e22e835ea9371844d5ca533f7d22640fd057859
alya <alyaggomaa@gmail.com>
2023-09-20 14:09:21 +0300

    calculator: start the calculator with a specific tool, predefined actual and predicted lists (necessary for with tw comparison)

--- metrics/calculator.py
+++ metrics/calculator.py
@@ -1,7 +1,6 @@
 from database.sqlite_db import SQLiteDB
 from termcolor import colored
 from sklearn.metrics import confusion_matrix
-from typing import Tuple, List
 from os import path
 from math import sqrt
 
@@ -9,9 +8,19 @@ class Calculator:
     name = "MetricsCalculator"
     # will save the tp, tn, fp and fn for each tool in this dict
     metrics = {}
-    def __init__(self, output_dir: str):
+    
+    def __init__(self,
+                 tool,
+                 actual_labels:list,
+                 predicted_labels: list,
+                 output_dir: str
+                 ):
         self.db = SQLiteDB(output_dir)
         self.results_file = path.join(output_dir, 'results.txt')
+        assert tool in ['slips', 'suricata'], f'Trying to get metrics of an invalid tool: {tool}'
+        self.tool = tool
+        self.actual_labels = actual_labels
+        self.predicted_labels = predicted_labels
 
     def log(self, green_txt, normal_txt):
         normal_txt = str(normal_txt)
@@ -22,28 +31,6 @@ class Calculator:
         with open(self.results_file, 'a') as results:
             results.write(f"[{self.name}] {green_txt} {normal_txt}\n")
 
-
-    def get_labels_list(self, tool) -> Tuple[List, List]:
-        """
-        parses the labels from the db and returns actual and predicted labels list
-        :param tool: the tool we wanna retrieve the labels of. can be either slips or suricata.
-        :return: a tuple with 2 lists, first is actual, second is predicted
-        """
-        actual = []
-        predicted = []
-
-        # get all the ground truth labels
-        for flow in self.db.get_labeled_flows_by('ground_truth'):
-            # each flow looks something like this
-            # ('1:Vdr6nTTZvru6dIeEb/SYh9dxtCI=', 'benign', None, None)
-            aid, ground_truth_label, slips_label, suricata_label = flow
-            actual.append(ground_truth_label)
-            # this is important. if any of the tools have no label for a specific flow, we consider it as benign
-            if tool == 'slips':
-                predicted.append(slips_label)
-            elif tool =='suricata':
-                predicted.append(suricata_label)
-        return (actual, predicted)
 
     def clean_labels(self, labels: list)-> list:
         """
@@ -56,17 +43,13 @@ class Calculator:
                 labels[idx] = 'benign'
         return labels
 
-    def get_confusion_matrix(self, tool:str):
+    def get_confusion_matrix(self):
         """
-        prints the FP, FN, TP, TN of the given tool compared with the ground truth
-        :param tool: 'slips' or 'suricata'
+        prints the FP, FN, TP, TN of the given self.tool compared with the ground truth
         """
-        assert tool in ['slips', 'suricata'], f'Trying to get FP rate of invalid tool: {tool}'
 
-        actual, predicted = self.get_labels_list(tool)
-
-        actual: list = self.clean_labels(actual)
-        predicted: list = self.clean_labels(predicted)
+        actual: list = self.clean_labels(self.actual_labels)
+        predicted: list = self.clean_labels(self.predicted_labels)
 
         # the order of labels is Negative, Positive respectively.
         cm = confusion_matrix(actual, predicted, labels=['benign', 'malicious'])
@@ -76,33 +59,33 @@ class Calculator:
         fp = cm[0, 1]
         fn = cm[1, 0]
 
-        self.log(f"{tool}: True Positives (TP): ", tp)
-        self.log(f"{tool}: True Negatives (TN): ", tn)
-        self.log(f"{tool}: False Positives (FP): ", fp)
-        self.log(f"{tool}: False Negatives (FN): ", fn)
+        self.log(f"{self.tool}: True Positives (TP): ", tp)
+        self.log(f"{self.tool}: True Negatives (TN): ", tn)
+        self.log(f"{self.tool}: False Positives (FP): ", fp)
+        self.log(f"{self.tool}: False Negatives (FN): ", fn)
         print()
+        
         # will use them later
-        self.metrics[tool] = {
+        self.metrics[self.tool] = {
             'TP': tp,
             'TN': tn,
             'FP': fp,
             'FN': fn
         }
-        self.db.store_confusion_matrix(tool, self.metrics[tool])
+        self.db.store_confusion_matrix(self.tool, self.metrics[self.tool])
 
-    def MCC(self, tool: str):
+    def MCC(self):
         """
         Calculates the Matthews correlation coefficient (MCC) for a given tool
-        :param tool: 'slips' or 'suricata'
         """
-        numerator = self.metrics[tool]['TP'] * self.metrics[tool]['TN'] \
-                    - self.metrics[tool]['FP'] * self.metrics[tool]['FN']
+        numerator = self.metrics[self.tool]['TP'] * self.metrics[self.tool]['TN'] \
+                    - self.metrics[self.tool]['FP'] * self.metrics[self.tool]['FN']
 
         denominator = sqrt(
-            (self.metrics[tool]['TP'] + self.metrics[tool]['FP'])
-            * (self.metrics[tool]['TP'] + self.metrics[tool]['FN'])
-            * (self.metrics[tool]['TN'] + self.metrics[tool]['FP'])
-            * (self.metrics[tool]['TN'] + self.metrics[tool]['FN'])
+            (self.metrics[self.tool]['TP'] + self.metrics[self.tool]['FP'])
+            * (self.metrics[self.tool]['TP'] + self.metrics[self.tool]['FN'])
+            * (self.metrics[self.tool]['TN'] + self.metrics[self.tool]['FP'])
+            * (self.metrics[self.tool]['TN'] + self.metrics[self.tool]['FN'])
         )
 
         if denominator == 0:
@@ -112,148 +95,136 @@ class Calculator:
 
         return  mcc
 
-
-
-    def recall(self, tool: str):
+    def recall(self):
         """
         prints the recall of the given tool compared with the ground truth
-        :param tool: 'slips' or 'suricata'
         """
         # make sure we have the fp and tn of this store calculated already
-        if  tool not in self.metrics:
-            self.get_confusion_matrix(tool)
+        if  self.tool not in self.metrics:
+            self.get_confusion_matrix()
 
-        if self.metrics[tool]['TP'] + self.metrics[tool]['FN'] == 0:
-            self.log(f"Can't get recall of {tool} because TP+FN of {tool} is: "," 0")
+        if self.metrics[self.tool]['TP'] + self.metrics[self.tool]['FN'] == 0:
+            self.log(f"Can't get recall of {self.tool} because TP+FN of {self.tool} is: "," 0")
             recall = 0
         else:
-            recall = self.metrics[tool]['TP']/(self.metrics[tool]['TP'] + self.metrics[tool]['FN'])
+            recall = self.metrics[self.tool]['TP']/(self.metrics[self.tool]['TP'] + self.metrics[self.tool]['FN'])
 
-        self.metrics[tool].update({'recall': recall})
-        self.log(f"{tool}: recall: ", recall)
+        self.metrics[self.tool].update({'recall': recall})
+        self.log(f"{self.tool}: recall: ", recall)
         return recall
 
 
-    def precision(self, tool: str):
+    def precision(self):
         """
         prints the precision of the given tool compared with the ground truth
-        :param tool: 'slips' or 'suricata'
         """
         # make sure we have the fp and tn of this store calculated already
-        if  tool not in self.metrics:
-            self.get_confusion_matrix(tool)
+        if  self.tool not in self.metrics:
+            self.get_confusion_matrix()
 
-        if self.metrics[tool]['TP'] + self.metrics[tool]['FP'] == 0:
+        if self.metrics[self.tool]['TP'] + self.metrics[self.tool]['FP'] == 0:
             precision = 0
         else:
-            precision = self.metrics[tool]['TP']/(self.metrics[tool]['TP'] + self.metrics[tool]['FP'])
+            precision = self.metrics[self.tool]['TP']/(self.metrics[self.tool]['TP'] + self.metrics[self.tool]['FP'])
 
 
-        self.metrics[tool].update({'precision': precision})
-        self.log(f"{tool}: precision: ", precision)
+        self.metrics[self.tool].update({'precision': precision})
+        self.log(f"{self.tool}: precision: ", precision)
         return precision
 
-    def F1(self, tool):
+    def F1(self):
         """
         prints the F1 of the given tool
-        :param tool: 'slips' or 'suricata'
         """
-        if tool not in self.metrics:
-            self.get_confusion_matrix(tool)
+        if self.tool not in self.metrics:
+            self.get_confusion_matrix()
 
 
-        precision = self.metrics[tool]['precision']
-        recall = self.metrics[tool]['recall']
+        precision = self.metrics[self.tool]['precision']
+        recall = self.metrics[self.tool]['recall']
         if precision + recall == 0:
             f1 = 0
         else:
             f1 = (2 * precision * recall) / (precision + recall)
 
-        self.log(f"{tool}: F1: ", f1)
+        self.log(f"{self.tool}: F1: ", f1)
         return f1
 
 
 
-    def FPR(self, tool, log=True) -> float:
+    def FPR(self, log=True) -> float:
         """
         prints the false positive rate of a given tool
-        :param tool: slips or suricata
         :param log: logs the output to cli if set to true, we set it to false when we're using this function inside another one
         :return: float
         """
         # make sure we have the fp and tn of this store calculated already
-        if not tool in self.metrics:
-            self.get_confusion_matrix(tool)
+        if not self.tool in self.metrics:
+            self.get_confusion_matrix()
 
-        if self.metrics[tool]['FP'] + self.metrics[tool]['TN'] == 0:
+        if self.metrics[self.tool]['FP'] + self.metrics[self.tool]['TN'] == 0:
             fpr = 'none'
         else:
-            fpr = self.metrics[tool]['FP']/(self.metrics[tool]['FP'] + self.metrics[tool]['TN'])
+            fpr = self.metrics[self.tool]['FP']/(self.metrics[self.tool]['FP'] + self.metrics[self.tool]['TN'])
 
         if log:
-            self.log(f"{tool}: FPR: ", fpr)
+            self.log(f"{self.tool}: FPR: ", fpr)
 
         return fpr
 
-    def TPR(self, tool, log=True):
+    def TPR(self, log=True):
         """
         TPR = TP / (TP + FN)
         prints the true positive rate of a given tool
-        :param tool: slips or suricata
         :param log: logs the output to cli if set to true, we set it to false when we're using this function inside another one
         :return: float
         """
-        if self.metrics[tool]['TP'] + self.metrics[tool]['FN'] == 0:
+        if self.metrics[self.tool]['TP'] + self.metrics[self.tool]['FN'] == 0:
             tpr = 0
         else:
-            tpr = self.metrics[tool]['TP'] / (self.metrics[tool]['TP'] + self.metrics[tool]['FN'])
+            tpr = self.metrics[self.tool]['TP'] / (self.metrics[self.tool]['TP'] + self.metrics[self.tool]['FN'])
         if log:
-            self.log(f"{tool}: TPR: ", tpr)
+            self.log(f"{self.tool}: TPR: ", tpr)
         return tpr
 
-    def FNR(self, tool):
+    def FNR(self):
         """
         FNR = 1- TPR
         prints the false negative rate of a given tool
-        :param tool: slips or suricata
         :return: float
         """
-        fnr = 1 - self.TPR(tool, log=False)
-        self.log(f"{tool}: FNR: ", fnr)
+        fnr = 1 - self.TPR(log=False)
+        self.log(f"{self.tool}: FNR: ", fnr)
         return fnr
 
-    def TNR(self, tool, log=True):
+    def TNR(self, log=True):
         """
         FNR = 1 − FPR
         prints the true negative rate of a given tool
-        :param tool: slips or suricata
         :param log: logs the output to cli if set to true, we set it to false when we're using this function inside another one
         :return: float
         """
-        tnr = 1 - self.FPR(tool, log=False)
+        tnr = 1 - self.FPR(log=False)
         if log:
-            self.log(f"{tool}: TNR: ", tnr)
+            self.log(f"{self.tool}: TNR: ", tnr)
         return tnr
 
-
-
-    def accuracy(self, tool):
+    def accuracy(self):
         """
-        :param tool: slips or suricata
         :return: float
         """
-        numerator = self.metrics[tool]['TP'] + \
-                    self.metrics[tool]['TN']
+        numerator = self.metrics[self.tool]['TP'] + \
+                    self.metrics[self.tool]['TN']
 
-        denominator = self.metrics[tool]['TP'] + \
-                      self.metrics[tool]['TN'] + \
-                      self.metrics[tool]['FP'] + \
-                      self.metrics[tool]['FN']
+        denominator = self.metrics[self.tool]['TP'] + \
+                      self.metrics[self.tool]['TN'] + \
+                      self.metrics[self.tool]['FP'] + \
+                      self.metrics[self.tool]['FN']
         if denominator == 0:
             acc = 'none'
         else:
             acc = numerator / denominator
-        self.log(f"{tool}: Accuracy: ", acc)
+        self.log(f"{self.tool}: Accuracy: ", acc)
         return acc
 
     def __del__(self):
