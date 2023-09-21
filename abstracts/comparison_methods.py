c0f36894efa0cf5f57ca6646e3f96b8e99dfe73d
alya <alyaggomaa@gmail.com>
2023-09-21 15:28:25 +0300

    move each interface to its own file

---
+++ abstracts/comparison_methods.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+from multiprocessing import Process
+
+from database.sqlite_db import SQLiteDB
+
+
+class ComparisonMethod(ABC):
+    name = ''
+    def __init__(self,
+                 output_dir,
+                 **kwargs):
+        Process.__init__(self)
+        self.db = SQLiteDB(output_dir)
+        self.init(**kwargs)
+
+    @abstractmethod
+    def init(self, **kwargs):
+        """
+        the goal of this is to have one common __init__()
+        for all modules, which is the one in this file
+        this init will have access to all keyword args passes
+         when initializing the module
+        """
+
+    def log(self, green_txt, normal_txt):
+        """
+        logs the txt to stdout
+        """
+        end = '\n'
+        print(f"{colored(f'[{self.name}] ', 'blue')} "
+              f"{colored(green_txt, 'green')} "
+              f"{normal_txt}",
+              end=end)
+
+    def __del__(self):
+        self.db.close()
--- abstracts/parsers.py
+++ abstracts/parsers.py
@@ -1,7 +1,8 @@
+from abc import ABC, abstractmethod
 from multiprocessing import Process
-from database.sqlite_db import SQLiteDB
 from termcolor import colored
-from abc import abstractmethod, ABC
+from database.sqlite_db import SQLiteDB
+
 
 class Parser(ABC):
     name = ''
@@ -37,36 +38,3 @@ class Parser(ABC):
 
     def __del__(self):
         self.db.close()
-
-
-class ComparisonMethod(ABC):
-    name = ''
-    def __init__(self,
-                 output_dir,
-                 **kwargs):
-        Process.__init__(self)
-        self.db = SQLiteDB(output_dir)
-        self.init(**kwargs)
-
-    @abstractmethod
-    def init(self, **kwargs):
-        """
-        the goal of this is to have one common __init__()
-        for all modules, which is the one in this file
-        this init will have access to all keyword args passes
-         when initializing the module
-        """
-
-    def log(self, green_txt, normal_txt):
-        """
-        logs the txt to stdout
-        """
-        end = '\n'
-        print(f"{colored(f'[{self.name}] ', 'blue')} "
-              f"{colored(green_txt, 'green')} "
-              f"{normal_txt}",
-              end=end)
-
-    def __del__(self):
-        self.db.close()
-
