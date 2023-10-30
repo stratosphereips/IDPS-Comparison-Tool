4b45c2fb5367ce719bccc4e24109a39db4b05981
alya <alyaggomaa@gmail.com>
2023-10-30 13:34:28 +0200

    use aid_hash lib from pypi

--- utils/hash.py
+++ utils/hash.py
@@ -1,54 +1,40 @@
-from datetime import datetime, timezone
-from hashlib import sha1
-from base64 import b64encode
-import communityid
+import aid_hash
 
 from .timestamp_handler import TimestampHandler
 
 class Hash:
     def __init__(self):
-        self.community_id = communityid.CommunityID()
+        self.aid = aid_hash.AID()
         self.time_handler = TimestampHandler()
 
 
     def get_aid(self, flow: dict):
         """
-        calculates the flow SHA1(cid+ts) aka All-ID of the flow
-        because we need the flow ids to be unique to be able to compare them
+        calculates the  AID hash of the flow aka All-ID of the flow
         """
-        #TODO document this
-        community_id = self.get_community_id(flow)
+        # aid_hash lib only accepts unix ts
         ts: str = flow['timestamp']
         ts: str = self.time_handler.assert_microseconds(ts)
 
-        aid = f"{community_id}-{ts}"
-
-        # convert the input string to bytes (since hashlib works with bytes)
-        aid: str = sha1(aid.encode('utf-8')).hexdigest()
-        aid: str = b64encode(aid.encode()).decode()
-        return aid
-
-
-    def get_community_id(self, flow: dict):
-        """
-        calculates the flow community id of the given flow
-        """
         cases = {
-            'tcp': communityid.FlowTuple.make_tcp,
-            'udp': communityid.FlowTuple.make_udp,
-            'icmp': communityid.FlowTuple.make_icmp,
+            'tcp': aid_hash.FlowTuple.make_tcp,
+            'udp': aid_hash.FlowTuple.make_udp,
+            'icmp': aid_hash.FlowTuple.make_icmp,
         }
 
         try:
             proto = flow['proto'].lower()
 
             if 'icmp' in proto:
-                tpl = cases['icmp'](flow['saddr'], flow['daddr'], flow['type'], flow['code'])
+                tpl = cases['icmp'](ts, flow['saddr'], flow['daddr'], flow['type'], flow['code'])
             else:
-                tpl = cases[proto](flow['saddr'], flow['daddr'], flow['sport'], flow['dport'])
+                tpl = cases[proto](ts, flow['saddr'], flow['daddr'], flow['sport'], flow['dport'])
 
-            return self.community_id.calc(tpl)
+            return self.aid.calc(tpl)
         except (KeyError, TypeError):
-            # proto doesn't have a community_id.FlowTuple  method
+            # proto doesn't have an aid.FlowTuple  method
             return ''
+
+
+
 
