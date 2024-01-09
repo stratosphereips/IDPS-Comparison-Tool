5543e16711445b1a2667075c3ec57b63a039208a
alya <alyaggomaa@gmail.com>
2024-01-09 15:52:15 +0200

    add scripts/extractor.py  - extracts the highest accumulated threat level of each time window in the given alerts.json

---
+++ scripts/extractor.py
@@ -0,0 +1,122 @@
+"""
+This script extracts the highest accumulated threat level of each time
+window in the given alerts.json. it only prints timewindows of the
+ given host ip
+
+Usage:
+python3 extractor.py <slips' alerts.json> <host ip>
+
+"""
+
+
+import json
+import sys
+import ipaddress
+
+def count_and_print_duplicate_scores(scores: list):
+    """
+    prints x -- y times for duplicate continuous scores found in each twid
+    """
+    ctr = 0
+    printed = False
+    last_score_in_twid = False
+
+    for i in range(len(scores)):
+        score = float(scores[i])
+
+        try:
+            prev_score = float(scores[i-1])
+        except IndexError:
+            # cur score is the first one
+            prev_score = float(0)
+            last_score_in_twid = False
+
+
+
+        try:
+            nxt_score = float(scores[i+1])
+        except IndexError:
+            # cur score is the last one
+            nxt_score = 999999999999
+            last_score_in_twid = True
+
+        #######################################
+
+        if score != prev_score:
+            # reset the zeros ctr
+
+            if (not printed and ctr > 0):
+                print(f"{prev_score} --  {ctr} times")
+                printed = True
+                ctr = 0
+            if score != nxt_score:
+                print(score)
+                # bcus if it was = next score, we'll be counting them
+            else:
+                ctr = 1
+        elif score == prev_score:
+            # consequent zeros
+            ctr +=1
+            printed = False
+
+        if last_score_in_twid:
+            print(f"{prev_score} --  {ctr} times")
+
+
+
+def print_max_accumulated_score(scores: list):
+
+    print(max(scores))
+
+
+def get_ip_version(srcip):
+    # determine th eversion of the given IP
+    try:
+        ipaddress.IPv4Address(srcip)
+        ip_version = "IP4"
+    except ipaddress.AddressValueError:
+        ip_version = "IP6"
+    return ip_version
+
+
+
+tws = {}
+alertsjson = sys.argv[1]
+srcip = sys.argv[2]
+
+ip_version: str = get_ip_version(srcip)
+
+with open(alertsjson, 'r') as f:
+    lines_ctr = 0
+    while line:= f.readline():
+        lines_ctr += 1
+        line: dict = json.loads(line)
+        try:
+            sip = line["Source"][0][ip_version][0]
+        except Exception as e:
+            # detection doesn't match the given ipv4, skip it
+            continue
+
+        if sip != srcip:
+            continue
+
+        tl = line['accumulated_threat_level']
+        twid = line['timewindow']
+
+        if twid not in tws:
+            tws.update({twid :  [tl]})
+        else:
+            tws[twid].append(tl)
+
+
+#print(f"total alerts.json lines read: {lines_ctr}")
+
+sorted_dict = dict(sorted(tws.items()))
+
+
+for twid, scores in sorted_dict.items():
+    print(f"\n\ntimewindow {twid}\n")
+
+    #count_and_print_duplicate_scores(scores)
+
+    print_max_accumulated_score(scores)
